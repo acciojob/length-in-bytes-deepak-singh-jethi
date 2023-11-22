6d3db74809905e30951fb81d3b2dabdee20820b4
DEEPAK SINGH JETHI <134581381+deepak-singh-jethi@users.noreply.github.com>
2023-11-22 11:56:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const byteSize = (str) => {
   
-	 // Encode the string using UTF-8 encoding
-    encoded_string = str.encode('utf-8');
-	
-    return len(encoded_string)
+	// Encode the string using UTF-8 encoding
+  const encodedString = new TextEncoder().encode(inputString);
+  
+  // Return the size of the encoded string in bytes
+  return encodedString.length;
 	
 };
 
 // Do not change the code below
-const str = prompt("Enter some string.");
+const str = rompt("Enter some string.");
 alert(byteSize(str));
